1fa2cb4d45dc80c24613913b0ac181c629bbf8b5
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-08-06 10:51:07 +0200

    Create express-schoko.js

---
+++ module-2/http/express-schoko.js
@@ -0,0 +1,14 @@
+/*
+Aufgabe 1: "Das verrückte Schokoladenrezept"
+Erstelle eine Express-Anwendung, die einen Endpunkt /schokolade bereitstellt. Wenn jemand diesen Endpunkt aufruft, soll die Antwort eine zufällige Schokoladenrezeptidee sein, die aus einer Liste von verrückten Rezepten ausgewählt wird. Zum Beispiel:
+
+"Schokoladenkekse mit Erdnussbutter und Zuckerguss"
+"Schokoladenpudding mit einem Geheimnis"
+"Schokoladenfondue mit Zuckerwatte"
+
+*/
+
+// 1. Erstelle Express server mit .listen()
+// 2. Erstelle ein endpoint /schokolade
+// 3. Erstelle eine Liste mit Schokoladenrezeptideen
+// 4. Beim Aufruf von /schokolade sende eine zufällige Idee aus der Liste s. Punkt 3
