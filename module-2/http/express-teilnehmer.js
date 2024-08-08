0ab98b5a3b07ee8537dd7e001f5e377b828efafd
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-08-08 11:52:01 +0200

    Update express-teilnehmer.js

--- module-2/http/express-teilnehmer.js
+++ module-2/http/express-teilnehmer.js
@@ -39,3 +39,31 @@ function handleListen() {
 }
 
 app.listen(3000, handleListen)
+
+/*
+Zusammenfassung:
+
+1. wir haben eine einfache express-app erstellt
+2. haben eine ec2 maschine erstellt
+2.1 mit userdata script 
+#!/bin/bash
+apt-get update
+apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get install -y nodejs
+3. danach haben wir uns auf die maschine eingeloggt
+4. wir haben auf der maschine ein verzeichnis server erstellt
+5. danach haben wir index.js auf die maschine kopiert mit scp in den server ordner
+scp -i ~/.ssh/universal.pem ./index.js ubuntu@ec2-52-91-10-225.compute-1.amazonaws.com:/home/ubuntu/server
+6. wir haben die app gestartet mit nohup befehl
+nohup node index.js > output.log 2>&1 &
+7. danach haben wir einen weiteren endpoint erstellt
+8. wir haben eine get methode erstellt, die alle teilnehmer zurückgibt
+9. wir haben eine post methode erstellt, die einen neuen teilnehmer hinzufügt
+10. wir haben die index.js von local auf die ec2 maschine kopiert, mit scp siehe punkt 5
+11. zuerst mussten wir rausfinden wie die <pid> der app ist, mit ps aux | grep 'node index.js'
+12. wir mussten die app neu starten, mit kill -9 <pid> and nohup node index.js > output.log 2>&1 &
+13. wir haben die app auf port 3000 gestartet 
+14. that's it 🎉
+*/
